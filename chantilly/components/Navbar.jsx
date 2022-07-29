c9e07de7c418e560693b9eb14af7731f6f630dfa
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2022-07-29 11:10:59 +0800

    layout

--- chantilly/components/Navbar.jsx
+++ chantilly/components/Navbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">The Chantilly by John</Link>
+      </p>
+
+      <button className="cart-icon" type="button" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">
+          1
+        </span>
+      </button>
+    </div>
+  );
 }
 
 export default Navbar
